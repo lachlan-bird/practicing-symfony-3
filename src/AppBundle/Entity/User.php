955146250d6c3b90f5f86f0d773b438304daca9a
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-15 20:06:00 +1000

    Add roles array to database. Can be refactored out into a many-to-many relationship

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -29,6 +29,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $roles;
+
     private $plainPassword;
 
     public function getUsername()
@@ -38,7 +43,14 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        $roles = $this->roles;
+
+        if(!in_array('ROLE_USER', $roles))
+        {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return $roles;
     }
 
     public function getPassword()
@@ -82,5 +94,8 @@ class User implements UserInterface
         $this->password = null;
     }
 
-
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
 }
