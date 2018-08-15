09cfc2f6d37d10e54ed3f0b7abce1646fe0233a1
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-15 20:44:21 +1000

    Add validation to the register form

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,12 +2,15 @@
 
 namespace AppBundle\Entity;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
+ * @UniqueEntity(fields={"email"}, message="Looks like you already have an account")
  */
 class User implements UserInterface
 {
@@ -20,6 +23,8 @@ class User implements UserInterface
     private $id;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
      * @ORM\Column(type="string", unique=true)
      */
     private $email;
@@ -34,6 +39,9 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @Assert\NotBlank(groups={"Registration"})
+     */
     private $plainPassword;
 
     public function getUsername()
