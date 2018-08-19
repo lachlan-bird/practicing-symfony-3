a174877a8a3b41ee1e770379b45c746fad7d3e76
Lachlan Bird <lachlan.bird88@gmail.com>
2018-08-19 17:18:07 +1000

    Install server and fix some regressions

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -4,13 +4,13 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\Genus;
 use AppBundle\Form\GenusFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Security("is_granted('ROLE_MANAGE_GENUS')")
+ * @IsGranted("ROLE_MANAGE_GENUS")
  * @Route("/admin")
  */
 class GenusAdminController extends Controller
